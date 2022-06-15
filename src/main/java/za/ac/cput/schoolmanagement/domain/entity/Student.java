bac2255f8eafd2b09ddd552fb10a7c9606629297
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-06-15 04:38:38 +0200

    Creation of the both domain student and student address entities and the file comment line

---
+++ src/main/java/za/ac/cput/schoolmanagement/domain/entity/Student.java
@@ -0,0 +1,9 @@
+package za.ac.cput.schoolmanagement.domain.entity;
+/* Student.java
+   Student class for student management system
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 14 June 2022
+ */
+
+public class Student {
+}
---
+++ src/main/java/za/ac/cput/schoolmanagement/domain/entity/StudentAddress.java
@@ -0,0 +1,9 @@
+package za.ac.cput.schoolmanagement.domain.entity;
+/* StudentAddress.java
+   Student address class for student management system
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 14 June 2022
+ */
+
+public class StudentAddress {
+}

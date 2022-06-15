3f244d74c916578b403a29e6047d70c05516815f
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-06-15 05:47:07 +0200

    Fully built student and student address domain

--- src/main/java/za/ac/cput/schoolmanagement/domain/entity/Student.java
+++ src/main/java/za/ac/cput/schoolmanagement/domain/entity/Student.java
@@ -5,5 +5,69 @@ package za.ac.cput.schoolmanagement.domain.entity;
    Date: 14 June 2022
  */
 
+import java.util.Objects;
+
 public class Student {
+    private final String studentId, email, name;
+
+    // Private constructor is required
+    private Student(Builder builder){
+        this.studentId = builder.studentId;
+        this.email = builder.email;
+        this.name = builder.name;
+
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static class Builder{
+        private String studentId, email, name;
+
+        public Student.Builder studentId(String studentId){
+            this.studentId = studentId;
+            return this;
+        }
+        public Student.Builder email(String email){
+            this.email = email;
+            return this;
+        }
+        public Student.Builder name(String name){
+            this.name = name;
+            return this;
+        }
+        public Student.Builder copy(Student student){
+            this.studentId = student.studentId;
+            this.email = student.email;
+            this.name = student.name;
+            return this;
+        }
+        public Student build(){return new Student(this);}
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Builder builder = (Builder) o;
+            return studentId.equals(builder.studentId) && email.equals(builder.email) && name.equals(builder.name);
+        }
+
+        @Override
+        public String toString() {
+            return "Builder{" +
+                    "studentId='" + studentId + '\'' +
+                    ", email='" + email + '\'' +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
 }
--- src/main/java/za/ac/cput/schoolmanagement/domain/entity/StudentAddress.java
+++ src/main/java/za/ac/cput/schoolmanagement/domain/entity/StudentAddress.java
@@ -5,5 +5,57 @@ package za.ac.cput.schoolmanagement.domain.entity;
    Date: 14 June 2022
  */
 
+import java.util.Objects;
+
 public class StudentAddress {
+    private final String studentId, address;
+
+    //Private constructor required
+    private StudentAddress(Builder builder){
+        this.studentId = builder.studentId;
+        this.address = builder.address;
+
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public static class Builder{
+        private String studentId, address;
+
+        public Builder studentId(String studentId){
+            this.studentId = studentId;
+            return this;
+        }
+        public Builder address(String address){
+            this.address = address;
+            return this;
+        }
+        public Builder copy(StudentAddress studentAddress){
+            this.studentId = studentAddress.studentId;
+            this.address = studentAddress.address;
+            return this;
+        }
+        public StudentAddress build(){return new StudentAddress(this);}
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Builder builder = (Builder) o;
+            return studentId.equals(builder.studentId) && address.equals(builder.address);
+        }
+
+        @Override
+        public String toString() {
+            return "Builder{" +
+                    "studentId='" + studentId + '\'' +
+                    ", address='" + address + '\'' +
+                    '}';
+        }
+    }
 }

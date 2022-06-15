1e072bf9014809e7c07e321a60f3eade4ebd479e
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-06-15 05:51:38 +0200

    Creation of the student and student address factory and comment them out both

---
+++ src/main/java/za/ac/cput/schoolmanagement/factory/entityfactory/StudentAddressFactory.java
@@ -0,0 +1,9 @@
+package za.ac.cput.schoolmanagement.factory.entityfactory;
+/* StudentAddressFactory.java
+   This is the Student address factory class for student management system
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 14 June 2022
+ */
+
+public class StudentAddressFactory {
+}
---
+++ src/main/java/za/ac/cput/schoolmanagement/factory/entityfactory/StudentFactory.java
@@ -0,0 +1,9 @@
+package za.ac.cput.schoolmanagement.factory.entityfactory;
+/* StudentFactory.java
+   This is the Student factory class for student management system
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 14 June 2022
+ */
+
+public class StudentFactory {
+}

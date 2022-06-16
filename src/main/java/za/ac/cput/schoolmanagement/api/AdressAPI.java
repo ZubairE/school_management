40237a25da8c3b3f5deb5e6ac0c7f9129e16e541
ZubairE <esau.zubair98@gmail.com>
2022-06-16 18:13:39 +0200

    Creating API and testing it

--- src/main/java/za/ac/cput/schoolmanagement/api/AdressAPI.java
+++ src/main/java/za/ac/cput/schoolmanagement/api/AdressAPI.java
@@ -1,4 +1,40 @@
 package za.ac.cput.schoolmanagement.api;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.cput.schoolmanagement.domain.entity.Adress;
+import za.ac.cput.schoolmanagement.service.entity.AdressService;
+
+import java.util.Optional;
+
+@Component
 public class AdressAPI {
+
+    private final AdressService adressService;
+
+    @Autowired
+    public AdressAPI(AdressService adressService, AdressService adressService1) {
+        //      this.adressService = adressService;
+        ///  }
+        this.adressService = adressService1;
+    }
+
+
+ public Adress save(Adress adress){
+     Optional<Adress> read = this.adressService.read(adress.getUnitNumber());
+     if (read.isPresent()){
+         Optional<Adress> read1 = this.adressService.read(adress.getUnitNumber());
+         if (read1.isPresent()){
+           //  Adress Adress = null;
+             return this.adressService.save(adress);
+         }
+     }
+
+        return null;
+ }
+
+
+
+
 }
--- src/test/java/za/ac/cput/schoolmanagement/api/AdressAPITest.java
+++ src/test/java/za/ac/cput/schoolmanagement/api/AdressAPITest.java
@@ -1,7 +1,33 @@
 package za.ac.cput.schoolmanagement.api;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.schoolmanagement.domain.entity.Adress;
+import za.ac.cput.schoolmanagement.factory.entityfactory.AdressFactory;
+import za.ac.cput.schoolmanagement.service.entity.AdressService;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
+@SpringBootTest
 class AdressAPITest {
+
+   @Autowired private AdressService adressService;
+    @Autowired private AdressAPI api;
+
+    private Adress adress = AdressFactory
+            .build("","","","", Integer.parseInt(""),"");
+
+
+    @Test
+    void save(){
+         Adress saved = this.api.save(this.adress);
+    }
+
+
+
+
 
 }

5a357a760fb6a45076490b4ad7afac5b8623dd49
Chadrack Mbuyi <chadrackmbuyi2@gmail.com>
2022-06-16 19:16:47 +0200

    Comment line added

--- src/main/java/za/ac/cput/schoolmanagement/repository/entity/StudentAddressRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/entity/StudentAddressRepository.java
@@ -1,4 +1,9 @@
 package za.ac.cput.schoolmanagement.repository.entity;
+/* StudentAddressRepository.java
+   Student address repository class for student management system
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 14 June 2022
+ */
 
 import za.ac.cput.schoolmanagement.domain.entity.Student;
 import za.ac.cput.schoolmanagement.domain.entity.StudentAddress;
--- src/main/java/za/ac/cput/schoolmanagement/repository/entity/StudentRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/entity/StudentRepository.java
@@ -1,4 +1,9 @@
 package za.ac.cput.schoolmanagement.repository.entity;
+/* StudentRepository.java
+   Student repository class for student management system
+   Author: Chadrack Mbuyi Kalala (219013012)
+   Date: 14 June 2022
+ */
 
 import za.ac.cput.schoolmanagement.domain.entity.Student;
 import za.ac.cput.schoolmanagement.repository.IRepository;

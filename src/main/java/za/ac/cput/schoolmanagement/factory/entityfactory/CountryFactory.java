b40ce8d64ac7bb12ba6e0b5e2a8dd9b5102a304d
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-06-17 04:19:58 +0200

    Updated CountryFactory & CountryFactoryTest

--- src/main/java/za/ac/cput/schoolmanagement/factory/entityfactory/CountryFactory.java
+++ src/main/java/za/ac/cput/schoolmanagement/factory/entityfactory/CountryFactory.java
@@ -5,9 +5,9 @@ import za.ac.cput.schoolmanagement.helper.HelpStrings;
 
 public class CountryFactory {
     public static Country build(String id, String name) {
-        if (id.isEmpty() || id == null)
+        if (id == null || id.isEmpty())
             throw new IllegalArgumentException("id is required");
-        if (name.isEmpty() || name == null)
+        if (name == null || name.isEmpty())
             throw new IllegalArgumentException("name is required");
         return new Country.Builder().id(id)
                 .name(name).build();
--- src/test/java/za/ac/cput/schoolmanagement/factory/entityfactory/CountryFactoryTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/factory/entityfactory/CountryFactoryTest.java
@@ -7,10 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CountryFactoryTest {
 
-    @Test public void build(){
+    @Test
+    public void build() {
         Country country = CountryFactory.build("87", "South Africa");
         System.out.println(country);
         assertNotNull(country);
     }
 
+    @Test
+    public void buildWithError() {
+
+        Exception e1 = assertThrows(IllegalArgumentException.class, () ->
+                CountryFactory.build(null, "South Africa"));
+        String exceptionMessage = e1.getMessage();
+        System.out.println(exceptionMessage);
+        assertSame("id is required", exceptionMessage);
+
+        Exception e2 = assertThrows(IllegalArgumentException.class, () ->
+                CountryFactory.build("87", null));
+        String exceptionMessage2 = e2.getMessage();
+        System.out.println(exceptionMessage2);
+        assertSame("name is required", exceptionMessage2);
+    }
 }

31639d6c1b8255c110938e3d4e7fac2530ab2397
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-06-17 05:41:44 +0200

    Added code to CityRepository & CountryRepository classes

--- src/main/java/za/ac/cput/schoolmanagement/repository/entity/CityRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/entity/CityRepository.java
@@ -1,4 +1,45 @@
 package za.ac.cput.schoolmanagement.repository.entity;
 
+import za.ac.cput.schoolmanagement.domain.entity.City;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class CityRepository {
+    private final List<City> cityList;
+    private static CityRepository CITY_REPOSITORY;
+
+    private CityRepository() {
+        this.cityList = new ArrayList<>();
+    }
+
+    public static CityRepository cityRepository() {
+        if (CITY_REPOSITORY == null)
+            CITY_REPOSITORY = new CityRepository();
+        return CITY_REPOSITORY;
+    }
+
+    public City save(City city) {
+        Optional<City> read = read(city.getId(), city.getName());
+        if (read.isPresent()) {
+            delete(read.get());
+        }
+        this.cityList.add(city);
+        return city;
+    }
+
+    public Optional<City> read(String id, String name) {
+        return this.cityList.stream().filter(c -> c.getId().equalsIgnoreCase(id))
+                .filter(city -> city.getName().equalsIgnoreCase(name))
+                .findFirst();
+    }
+
+    public void delete(City city) {
+        this.cityList.remove(city);
+    }
+
+    public List<City> findAll() {
+        return this.cityList;
+    }
 }
--- src/main/java/za/ac/cput/schoolmanagement/repository/entity/CountryRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/entity/CountryRepository.java
@@ -1,4 +1,46 @@
 package za.ac.cput.schoolmanagement.repository.entity;
 
+import za.ac.cput.schoolmanagement.domain.entity.City;
+import za.ac.cput.schoolmanagement.domain.entity.Country;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class CountryRepository {
+    private final List<Country> countryList;
+    private static CountryRepository COUNTRY_REPOSITORY;
+
+    private CountryRepository() {
+        this.countryList = new ArrayList<>();
+    }
+
+    public static CountryRepository countryRepository() {
+        if (COUNTRY_REPOSITORY == null)
+            COUNTRY_REPOSITORY = new CountryRepository();
+        return COUNTRY_REPOSITORY;
+    }
+
+    public Country save(Country country) {
+        Optional<Country> read = read(country.getId(), country.getName());
+        if (read.isPresent()) {
+            delete(read.get());
+        }
+        this.countryList.add(country);
+        return country;
+    }
+
+    public Optional<Country> read(String id, String name) {
+        return this.countryList.stream().filter(c -> c.getId().equalsIgnoreCase(id))
+                .filter(city -> city.getName().equalsIgnoreCase(name))
+                .findFirst();
+    }
+
+    public void delete(Country country) {
+        this.countryList.remove(country);
+    }
+
+    public List<Country> findAll() {
+        return this.countryList;
+    }
 }

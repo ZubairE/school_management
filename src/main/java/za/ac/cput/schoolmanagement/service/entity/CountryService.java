84399289648bb94b0eb54433112771930eb3cc76
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-06-17 19:21:19 +0200

    Created CountryService & ICountryService

--- src/main/java/za/ac/cput/schoolmanagement/service/entity/CountryService.java
+++ src/main/java/za/ac/cput/schoolmanagement/service/entity/CountryService.java
@@ -6,24 +6,30 @@ import za.ac.cput.schoolmanagement.repository.entity.CountryRepository;
 import za.ac.cput.schoolmanagement.service.entity.ICountryService;
 
 public class CountryService implements ICountryService {
-//    private final CountryRepository repository;
-//
-//    private CountryService (){
-//        this.repository =
-//    }
+    private final CountryRepository repository;
+    private static CountryService SERVICE;
 
+    private CountryService (){
+        this.repository = CountryRepository.countryRepository();
+    }
+
+    private static CountryService getService(){
+        if (SERVICE == null)
+            SERVICE = new CountryService();
+        return SERVICE;
+    }
     @Override
     public Country save(Country country){
-        return null;
+        return this.repository.save(country);
     }
 
     @Override
     public Optional<Country> read(String s){
-        return Optional.empty();
+        return this.repository.read(s);
     }
 
     @Override
     public void delete(Country country){
-
+        this.repository.delete(country);
     }
 }
--- src/main/java/za/ac/cput/schoolmanagement/service/entity/ICountryService.java
+++ src/main/java/za/ac/cput/schoolmanagement/service/entity/ICountryService.java
@@ -4,5 +4,4 @@ import za.ac.cput.schoolmanagement.domain.entity.Country;
 import za.ac.cput.schoolmanagement.service.IService;
 
 public interface ICountryService extends IService<Country, String> {
-
 }

2e0c7e5541a44b55f450473fb32ed331d10f3d54
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-06-17 19:25:43 +0200

    Created and populated CountryService & ICountryService

---
+++ src/main/java/za/ac/cput/schoolmanagement/service/entity/CityService.java
@@ -0,0 +1,36 @@
+package za.ac.cput.schoolmanagement.service.entity;
+
+import za.ac.cput.schoolmanagement.domain.entity.City;
+import za.ac.cput.schoolmanagement.repository.entity.CityRepository;
+
+import java.util.Optional;
+
+public class CityService implements ICityService{
+    private final CityRepository repository;
+    private static CityService SERVICE;
+
+    private CityService (){
+        this.repository = CityRepository.cityRepository();
+    }
+
+    private static CityService getService(){
+        if (SERVICE == null)
+            SERVICE = new CityService();
+        return SERVICE;
+    }
+    @Override
+    public City save(City city){
+        return this.repository.save(city);
+    }
+
+    @Override
+    public Optional<City> read(String s){
+        return this.repository.read(s);
+    }
+
+    @Override
+    public void delete(City city){
+        this.repository.delete(city);
+    }
+}
+
---
+++ src/main/java/za/ac/cput/schoolmanagement/service/entity/ICityService.java
@@ -0,0 +1,7 @@
+package za.ac.cput.schoolmanagement.service.entity;
+
+import za.ac.cput.schoolmanagement.domain.entity.City;
+import za.ac.cput.schoolmanagement.service.IService;
+
+public interface ICityService extends IService<City, String> {
+}

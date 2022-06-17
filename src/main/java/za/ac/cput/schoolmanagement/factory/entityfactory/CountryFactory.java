cf721700c1176d24e9ea4fc04290a22a3ef55e9a
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-06-17 04:03:39 +0200

    Country Factory Test class updated

--- src/main/java/za/ac/cput/schoolmanagement/factory/entityfactory/CountryFactory.java
+++ src/main/java/za/ac/cput/schoolmanagement/factory/entityfactory/CountryFactory.java
@@ -1,4 +1,15 @@
 package za.ac.cput.schoolmanagement.factory.entityfactory;
 
+import za.ac.cput.schoolmanagement.domain.entity.Country;
+import za.ac.cput.schoolmanagement.helper.HelpStrings;
+
 public class CountryFactory {
+    public static Country build(String id, String name) {
+        if (id.isEmpty() || id == null)
+            throw new IllegalArgumentException("id is required");
+        if (name.isEmpty() || name == null)
+            throw new IllegalArgumentException("name is required");
+        return new Country.Builder().id(id)
+                .name(name).build();
+    }
 }

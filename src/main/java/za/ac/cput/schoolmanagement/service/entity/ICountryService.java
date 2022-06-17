c6496dba7e2b1ee25d070e0dff8e1179711e48ed
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-06-17 09:43:45 +0200

    Created CountryService & ICountryService

---
+++ src/main/java/za/ac/cput/schoolmanagement/service/entity/ICountryService.java
@@ -0,0 +1,8 @@
+package za.ac.cput.schoolmanagement.service.entity;
+
+import za.ac.cput.schoolmanagement.domain.entity.Country;
+import za.ac.cput.schoolmanagement.service.IService;
+
+public interface ICountryService extends IService<Country, String> {
+
+}

cf7510f81e3ff09a28e110f28a60bcf07a5a30ab
ZubairE <esau.zubair98@gmail.com>
2022-06-17 10:07:56 +0200

    Creating and testing AdressFactory and testing read of AdressService

--- src/test/java/za/ac/cput/schoolmanagement/service/entity/AdressServiceTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/service/entity/AdressServiceTest.java
@@ -2,23 +2,30 @@ package za.ac.cput.schoolmanagement.service.entity;
 
 import org.junit.jupiter.api.Test;
 
+import za.ac.cput.schoolmanagement.domain.entity.Adress;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdressServiceTest {
 
-    @Test
-    void save() {
-    }
+
+    private Adress adress;
+    private AdressService service;
+    private String unitNumber;
+
+
 
     @Test
     void read() {
+        Optional<Adress> read = this.service.read(this.unitNumber);
+        System.out.println(read);
+        assertAll(
+                () -> assertTrue(read.isPresent()),
+                () -> assertEquals(this.adress, read.get())
+        );
     }
 
-    @Test
-    void delete() {
-    }
 
-    @Test
-    void findAll() {
-    }
 }

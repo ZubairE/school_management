10cbe6e546fcff77597dbc69ec98e433bba05ac3
UwaisRawoot <uwaisrawoot2@gmail.com>
2022-06-18 19:49:31 +0200

    Created City & Country controllers and APIs and their test classes

---
+++ src/main/java/za/ac/cput/schoolmanagement/controller/CityController.java
@@ -0,0 +1,4 @@
+package za.ac.cput.schoolmanagement.controller;
+
+public class CityController {
+}
---
+++ src/test/java/za/ac/cput/schoolmanagement/controller/CountryControllerTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.schoolmanagement.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryControllerTest {
+
+}
